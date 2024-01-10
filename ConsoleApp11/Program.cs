620328d275c396ccdeb99176d6d99b79d365f550
ASUS <ASUS@LAPTOP-CT0AS4K5>
2024-01-10 16:46:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace SimpleCalculator.UnitTests
+{
+    [TestFixture]
+    public class CalculatorFormTests
+    {
+        [Test]
+        public void EvaluateExpression_Addition_ReturnsCorrectResult()
+        {
+            // Arrange
+            CalculatorForm calculatorForm = new CalculatorForm();
+            string expression = "5+3";
+
+            // Act
+            double result = calculatorForm.EvaluateExpression(expression);
+
+            // Assert
+            Assert.AreEqual(8, result);
+        }
+
+        [Test]
+        public void EvaluateExpression_DivisionByZero_ThrowsArgumentException()
+        {
+            // Arrange
+            CalculatorForm calculatorForm = new CalculatorForm();
+            string expression = "5/0";
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => calculatorForm.EvaluateExpression(expression));
+        }
+
+        // Другие тесты для остальных операций и сценариев
+    }
+}
